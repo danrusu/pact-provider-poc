d5e6dfeb1f6d5eaec20ecb07e39a3f2497962d44
danrusu <danginkgo@yahoo.com>
2023-09-01 12:18:52 +0300

    no healthcheck endpoint

--- src/app.js
+++ src/app.js
@@ -8,16 +8,7 @@ app.use(express.json());
 app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:1112' }));
 
 // routes
-app
-  .get('/health', healthCheck)
-  .get('/products', getProducts)
-  .get('/products/:productId', getProduct);
+app.get('/products', getProducts).get('/products/:productId', getProduct);
 
 const port = process.env.PORT || 1113;
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
-
-function healthCheck(_req, res) {
-  res.status(200).send({
-    healthy: true,
-  });
-}
